1f88965709d43f0af4c05d80aa0266475ea28448
ofcluan07 <luan_dr1@hotmail.com>
2019-07-23 22:01:07 -0300

    Criando o componente da tela Consultas, faltando apenas o corpo

---
+++ src/componentes/Consultas/Consultas.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class Consultas extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            "realizadas": [],
+            "marcadas": []
+        };
+    }
+
+    componentDidMount(){
+        fetch("http://www.devup.com.br/php/api-dashboard/api/consultas")
+        .then(resultado => resultado.json
+            .then(dados => this.setState(dados)
+            )
+            )
+    }
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+export default Consultas;
